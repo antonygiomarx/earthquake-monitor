44053241d4042164db50abc3abd712aacaa89621
Antony Giomar <antonygiomarx@gmail.com>
2020-06-22 01:10:07 -0600

    Min changes in nav

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,34 +3,32 @@ const flag = require("../../assets/img/icons/common/flag-of-nicaragua.png");
 const NavbarComponent = () => {
   return (
     <nav className="navbar navbar-top  navbar-expand navbar-dark bg-primary">
-      <div className="container-fluid ">
-        <div className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav  ml-auto ml-md">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link pr-0"
-                href="https://www.nicatechsolutions.com"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="media align-items-center">
-                  <span className="avatar avatar-sm rounded-circle">
-                    <img alt="" src={flag} />
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link pr-0"
+              href="https://www.nicatechsolutions.com"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="media align-items-center">
+                <span className="avatar avatar-sm rounded-circle">
+                  <img alt="" src={flag} />
+                </span>
+                <div className="media-body  ml-2  d-none d-lg-block">
+                  <span className="mb-0 text-sm  font-weight-bold text-uppercase">
+                    Nicaragua
                   </span>
-                  <div className="media-body  ml-2  d-none d-lg-block">
-                    <span className="mb-0 text-sm  font-weight-bold text-uppercase">
-                      Nicaragua
-                    </span>
-                  </div>
                 </div>
-              </a>
-            </li>
-          </ul>
-        </div>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
